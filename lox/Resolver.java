90ad465f948c7085d7a3547bdb2522a52e6981ad
rossignol <2746054243@qq.com>
2026-07-28 22:41:10 +0800

    feat: 实现类访问/方法/this

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -17,15 +17,23 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         this.interpreter = interpreter;
     }
 
+    private enum FunctionType {
+        NONE,
+        FUNCTION,
+        METHOD
+    }
+
+    private enum ClassType {
+        NONE,
+        CLASS
+    }
+
+    private ClassType currentClass = ClassType.NONE;
+
     void resolve(List<Stmt> statements){
         for (Stmt stmt : statements){
             resolve(stmt);
         }
-    }
-
-    private enum FunctionType {
-        NONE,
-        FUNCTION
     }
 
     @Override
@@ -38,8 +46,18 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     @Override
     public Void visitClassStmt(Stmt.Class stmt) {
+        ClassType enclosedClass = currentClass;
+        currentClass = ClassType.CLASS;
         declare(stmt.name);
         define(stmt.name);
+        beginScope();
+        scopes.peek().put("this", true);
+        for (Stmt.Function method : stmt.methods){
+            FunctionType declaration = FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
+        endScope();
+        currentClass = enclosedClass;
         return null;
     }
 
@@ -138,6 +156,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     }
 
     @Override
+    public Void visitGetExpr(Expr.Get expr) {
+        resolve(expr.object);
+        return null;
+    }
+
+    @Override
     public Void visitGroupingExpr(Expr.Grouping expr) {
         resolve(expr.expression);
         return null;
@@ -152,6 +176,24 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     public Void visitLogicalExpr(Expr.Logical expr) {
         resolve(expr.left);
         resolve(expr.right);
+        return null;
+    }
+
+    @Override
+    public Void visitSetExpr(Expr.Set expr) {
+        resolve(expr.value);
+        resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitThisExpr(Expr.This expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword,
+                    "Can't use 'this' outside of a class.");
+            return null;
+        }
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
